82640cd243abf8721b3a405407217cdf0f0c8c46
Guido Santillan <guidosantillan01@gmail.com>
2020-01-15 19:19:26 +0100

    Fix App import at index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import './styles/App.css';
-import './components/App';
+import App from './components/App';
 
 // Connect ApolloClient instance with the GraphQL API at localhost:4000
 const httpLink = createHttpLink({
