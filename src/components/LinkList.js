463c094a0f367ff95f7c2a2cb906f188d6292ad4
Guido Santillan <guidosantillan01@gmail.com>
2020-01-15 19:25:41 +0100

    Fetch data from Prisma Server

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
 import Link from './Link';
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`;
 
 class LinkList extends Component {
   render() {
-    const linksToRender = [
-      {
-        id: '1',
-        description: 'Prisma turns your database into a GraphQL API 😎',
-        url: 'https://www.prismagraphql.com'
-      },
-      {
-        id: '2',
-        description: 'The best GraphQL client',
-        url: 'https://www.apollographql.com/docs/react/'
-      }
-    ];
-
     return (
-      <div>
-        {linksToRender.map((link) => (
-          <Link key={link.id} link={link} />
-        ))}
-      </div>
+      // Notice that we’re returning linksToRender as a function result, that’s due to render prop function provided by <Query /> component.
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching...</div>;
+          if (error) return <div>Error...</div>;
+
+          const linksToRender = data.feed.links;
+
+          return (
+            <div>
+              {linksToRender.map((link) => (
+                <Link key={link.id} link={link} />
+              ))}
+            </div>
+          );
+        }}
+      </Query>
     );
   }
 }
