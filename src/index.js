9b845da051b9eac44952e21f1fa7ac53db03f188
Guido Santillan <guidosantillan01@gmail.com>
2020-01-16 11:29:16 +0100

    Configuring Apollo with the authentication token

--- src/index.js
+++ src/index.js
@@ -6,18 +6,32 @@ import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
 
 import './styles/App.css';
 import App from './components/App';
+
+import { AUTH_TOKEN } from './constants';
 
 // Connect ApolloClient instance with the GraphQL API at localhost:4000
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
 
+// This middleware will be invoked every time ApolloClient sends a request to the server.
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
 // Instantiate ApolloClient by passing in the httpLink and a new instance of an InMemoryCache
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
